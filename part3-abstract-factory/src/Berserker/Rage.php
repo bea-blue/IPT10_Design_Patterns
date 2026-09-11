a247b102be57fd2024ac70e9d2c3c6bb32622979
Bea Luise <uson.bealouise@student.auf.edu.ph>
2026-09-12 06:41:33 +0800

    Add Part 3 Abstract Factory: Berserker family

---
+++ part3-abstract-factory/src/Berserker/Rage.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+namespace Fantasy\Berserker;
+use Fantasy\Contracts\Ability;
+
+class Rage implements Ability
+{
+    public function activate(): string
+    {
+        return "RAGE! Frenzy takes over — unstoppable fury!";
+    }
+}
